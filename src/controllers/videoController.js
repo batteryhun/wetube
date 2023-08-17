14ae2f2714f05bd00a43ac4e26f1f3d74a2571ed
batteryhun <snuleex@gmail.com>
2023-08-17 11:01:09 +0900

    pugs practice

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,6 +1,42 @@
-export const trending = (req, res) => res.render("home");
+export const trending = (req, res) => {
+  const videos = [
+    {
+      title: "Poco",
+      rating: 5,
+      comments: 2,
+      createdAt: "2min ago",
+      views: 59,
+      id: 1,
+    },
+    {
+      title: "Onuel",
+      rating: 4,
+      comments: 3,
+      createdAt: "10min ago",
+      views: 12,
+      id: 2,
+    },
+    {
+      title: "Munji",
+      rating: 4,
+      comments: 3,
+      createdAt: "15min ago",
+      views: 140,
+      id: 3,
+    },
+    {
+      title: "Agi",
+      rating: 5,
+      comments: 45,
+      createdAt: "1hr ago",
+      views: 259,
+      id: 4,
+    },
+  ];
+  return res.render("home", { pageTitle: "Home", videos });
+};
 export const see = (req, res) => res.render("watch");
-export const edit = (req, res) => res.send("Video Edit");
+export const edit = (req, res) => res.render("edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => res.send("Delete Videos");
